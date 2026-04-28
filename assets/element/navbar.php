fe0c31004aa99013c41b187d217b0e53820fdda8
HoshiUkasyah <ukasyah010@gmail.com>
2026-04-28 10:30:45 +0700

    Add files via upload

---
+++ assets/element/navbar.php
@@ -0,0 +1,21 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link justify-content-center active" aria-current="page" href="../../index.php"><img src="../../assets/img/SOCIALIZ.png" alt="Logo" height="30"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link justify-content-center active" aria-current="page" href="../../tambah/tambah_posts.php">Tambah Postingan</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="../../auth/logout.php">Logout</a>
+        </li>
+    </div>
+  </div>
+</nav>
